3a9fe9a560448788fe4e6b2d2661c45b736a86e9
Denis Dymerlii <dum.denya15@gmail.com>
2024-04-18 15:05:45 +0300

    change type hints

--- scraping/spreadsheet.py
+++ scraping/spreadsheet.py
@@ -1,4 +1,5 @@
 from dataclasses import fields
+from typing import NoReturn
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -6,7 +7,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 from scraping.models import Person
 
 
-def upload_to_spreadsheet(data, json_file_path, spreadsheet_name):
+def upload_to_spreadsheet(data: [Person], json_file_path: str, spreadsheet_name: str) -> NoReturn:
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
     creds = ServiceAccountCredentials.from_json_keyfile_name(json_file_path, scope)
